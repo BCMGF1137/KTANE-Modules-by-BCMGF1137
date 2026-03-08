7cb43f0bf3b3d239503e3938e03b9fad28fb5c9c
BCMGF1137 <jonelcid@outlook.com>
2026-03-08 17:10:18 -0400

    Update U7-1: Simon's Scytale complete

--- Simon's Scytale/Assets/SimonsScytaleScript.cs
+++ Simon's Scytale/Assets/SimonsScytaleScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using System.Linq;
 using KModkit;
 using Rnd = UnityEngine.Random;
+using System.Text.RegularExpressions;
 
 public class SimonsScytaleScript : MonoBehaviour {
 
@@ -23,8 +24,18 @@ public class SimonsScytaleScript : MonoBehaviour {
 
     public Material[] lightTypes;
 
+    // Readonly variables
     private static readonly string _ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static readonly string[] _MODULES = "D,Simon's Sums,Cruel Boolean Wires,Modulo Maze,S,Terminology".Split(',');
+
+    // Keyboard support
+    private KeyCode[] TypableKeys =
+    {
+        KeyCode.R, KeyCode.Y, KeyCode.G, KeyCode.C, KeyCode.B, KeyCode.M
+    };
+
+    // IEnumerators
+    private IEnumerator sequence;
 
     // Initialize colors
     private static readonly string[] colorNames = {"Red", "Yellow", "Green", "Cyan", "Blue", "Magenta"};
@@ -42,20 +53,47 @@ public class SimonsScytaleScript : MonoBehaviour {
     private float rotateSpeed = 0f;
 
     private int[] validColors = new int[3];
+    private int[] keys;
     private int sequenceLength;
 
     private string colorSequence;
     private string answer;
 
+    private bool isInteractable = true; // Can we interact with the buttons?
+    private int inputCount = 0; // How many inputs have we submitted into the module?
+
+    private bool moduleSolved = false;
+    private float elapsed = 0f;
+
+    private bool moduleFocused = false; // Are we focused on the module? Necessary for keyboard support.
+    private bool twitchPlaysStruck = false;
+
+#pragma warning disable 414
+    private string TwitchHelpMessage = @"!{0} XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX [Submit a sequence of 30 colors. They MUST be letters from RGBCMY.]
+!{0} colors [List the colors on the module from top-left to bottom-right. Useful for colorblindness.]
+NOTE: This command dynamically changes depending on the number of flashes.";
+#pragma warning restore 414
+
     // ==Physical Methods==
 
     void Awake()
     {
+        _moduleID = _moduleIDCounter++;
+
+        GetComponent<KMSelectable>().OnFocus += delegate {
+            moduleFocused = true;
+        };
+        GetComponent<KMSelectable>().OnDefocus += delegate {
+            moduleFocused = false;
+        };
+
+        sequence = FlashSequence();
+
         foreach (KMSelectable button in buttons)
         {
             button.OnInteract += delegate ()
             {
-                //StartCoroutine(PressButton(button));
+                StartCoroutine(PressButton(button));
                 return false;
             };
             button.OnHighlight += delegate ()
@@ -76,7 +114,7 @@ public class SimonsScytaleScript : MonoBehaviour {
         rotateSpeed = Rnd.Range(1.5f, 2.2f);
 
         // Step 1: The Binary Sequence
-        bool[] binarySequence = Bomb.GetSerialNumber().Select(x => _ALPHABET.IndexOf(x) >= 5 && _ALPHABET.IndexOf(x) <= 20).ToArray();
+        bool[] binarySequence = Bomb.GetSerialNumber().Select(x => _ALPHABET.IndexOf(x) >= 5 && _ALPHABET.IndexOf(x) <= 21).ToArray();
         Debug.LogFormat("[Simon's Scytale #{0}] The initial binary sequence is {1}.", _moduleID, binarySequence.Select(x => x ? 1 : 0).Join(""));
 
         for (int i = 0; i < 6; i++)
@@ -259,7 +297,8 @@ public class SimonsScytaleScript : MonoBehaviour {
         bool generated = false;
         string initialSequence = _ALPHABET.Substring(0, sequenceLength);
 
-        int[] keys = new int[3];
+        keys = new int[3];
+        int[] origValidColors = validColors.Select(x => x).ToArray(); // Create new reference for validColors to prevent the below algorithm from exceptioning on attempt 2
 
         while (!generated)
         {
@@ -274,7 +313,7 @@ public class SimonsScytaleScript : MonoBehaviour {
             //Debug.LogFormat("[Simon's Scytale #{0}] The module is rotating {1}WARDS.", _moduleID, invertRotation ? "UP" : "DOWN");
 
             string predictSequence = initialSequence;
-            validColors = validColors.Select(x => x - 1).ToArray(); // Convert the positions of the valid colors to zero-based indices.
+            validColors = origValidColors.Select(x => x - 1).ToArray(); // Convert the positions of the valid colors to zero-based indices.
             if (invertRotation) validColors = validColors.Reverse().ToArray(); // Go from bottom to top if the rotation is inverted.
 
             /*
@@ -295,6 +334,7 @@ public class SimonsScytaleScript : MonoBehaviour {
 
             // Prevent the predicted sequence from doing nothing
             if (predictSequence != initialSequence) generated = true;
+            else Debug.LogFormat("[Simon's Scytale #{0}] Generation failed! Generating again...", _moduleID, modColors.Join(", "));
         }
 
         Debug.LogFormat("[Simon's Scytale #{0}] The module's colors are {1}.", _moduleID, modColors.Join(", "));
@@ -324,7 +364,27 @@ public class SimonsScytaleScript : MonoBehaviour {
 
         displays[1].text = "" + sequenceLength;
 
-        StartCoroutine(FlashSequence());
+        // Dynamic TP Help Message
+        string xrayCharacters = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX".Substring(0, sequenceLength);
+        TwitchHelpMessage = "!{0} " + xrayCharacters + " [Submit a sequence of " + sequenceLength + " colors. They MUST be letters from RGBCMY.]\n!{0} colors [List the colors on the module from top-left to bottom-right. Useful for colorblindness.]";
+
+        StartCoroutine(sequence);
+    }
+
+    private void GetNewAnswer() // Modify the answer after a strike.
+    {
+        colorSequence = answer;
+        Debug.LogFormat("[Simon's Scytale #{0}] The new flashing sequence is {1}.", _moduleID, colorSequence);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.LogFormat("[Simon's Scytale #{0}] Sequence after {1}: {2} -> {3}.", _moduleID, keys[i] > 0 ? "+" + keys[i] : "" + keys[i], answer, Scytale(answer, keys[i]));
+            answer = Scytale(answer, keys[i]);
+        }
+
+        Debug.LogFormat("[Simon's Scytale #{0}] The new submission sequence is {1}.", _moduleID, answer);
+
+        StartCoroutine(sequence);
     }
 
     // Flash the sequence.
@@ -367,16 +427,182 @@ public class SimonsScytaleScript : MonoBehaviour {
         }
     }
 
+    // Pressing a button
+    private IEnumerator PressButton(KMSelectable button)
+    {
+        StopCoroutine(sequence);
+
+        if (isInteractable)
+        {
+            bool struck = false;
+
+            inputCount++;
+            displays[0].text = ("" + inputCount).PadLeft(2, '0');
+
+            isInteractable = false;
+
+            button.AddInteractionPunch(0.2f);
+
+            Color32 colorStored;
+            colorStored = button.GetComponent<MeshRenderer>().material.color;
+            button.GetComponent<MeshRenderer>().material = lightTypes[1];
+            button.GetComponent<MeshRenderer>().material.color = colorStored;
+
+            // Check for incorrect input
+            string reqColor = colorNames["RYGCBM".IndexOf(answer[inputCount - 1])];
+            //Debug.LogFormat("[Simon's Scytale #{0}] The module requested {1}.", _moduleID, reqColor);
+            string inputColor = modColors[Array.IndexOf(buttons, button)];
+            //Debug.LogFormat("[Simon's Scytale #{0}] You pressed " + inputColor + ".", _moduleID);
+
+            Audio.PlaySoundAtTransform("Sound_" + inputColor, transform);
+
+            if (reqColor != inputColor)
+            {
+                struck = true;
+                twitchPlaysStruck = true;
+                Debug.LogFormat("[Simon's Scytale #{0}] For input {1}, the module requested {2}, but you pressed {3}. Strike!", _moduleID, inputCount, reqColor, inputColor);
+                Module.HandleStrike();
+            }
+            
+            yield return new WaitForSeconds(50f / 140f);
+
+            for (int i = 0; i < 6; i++)
+            {
+                colorStored = buttonMaterials[i].GetComponent<MeshRenderer>().material.color;
+                buttonMaterials[i].material = lightTypes[0];
+                buttonMaterials[i].GetComponent<MeshRenderer>().material.color = colorStored;
+            }
+
+            if (struck) // Change the answer on a strike.
+            {
+                yield return new WaitForSeconds(50f / 140f);
+                inputCount = 0;
+                GetNewAnswer();
+                isInteractable = true;
+            }
+            //else if (inputCount == 1) // Code for when the module is solved.
+            else if (inputCount == sequenceLength) // The submission has been exhausted. Module solved!
+            {
+                isInteractable = false;
+                moduleSolved = true;
+                Debug.LogFormat("[Simon's Scytale #{0}] You have correctly inputted all {1} colors. Solved!", _moduleID, sequenceLength);
+                yield return new WaitForSeconds(.3f);
+                StartCoroutine(SolveAnimation());
+            }
+            else
+            {
+                isInteractable = true;
+            }
+        }
+    }
+
     // When a button is highlighted, change the display to what is being highlighted.
     private void HLButton(KMSelectable button)
     {
-        int buttonIndex = Array.IndexOf(buttons, button);
-        displays[1].text = modColors[buttonIndex].Substring(0, 2).ToUpper();
+        if (!moduleSolved)
+        {
+            int buttonIndex = Array.IndexOf(buttons, button);
+            displays[1].text = modColors[buttonIndex].Substring(0, 2).ToUpper();
+        }
     }
 
     private void HLEndButton(KMSelectable button)
     {
+        if (!moduleSolved) displays[1].text = "" + sequenceLength;
+    }
+
+    // Solve animation
+    IEnumerator TimeFix()
+    {
+        while (true)
+        {
+            yield return null;
+            elapsed += Time.deltaTime;
+            //Debug.LogFormat("[Simon's Scytale #{0}] elapsed={1}.", _moduleID, elapsed);
+        }
+    }
+
+    IEnumerator SolveAnimation()
+    {
+        StartCoroutine(TimeFix());
+        Audio.PlaySoundAtTransform("Solve", transform);
+
+        Color32 colorStored;
+        displays[0].text = "" + sequenceLength;
         displays[1].text = "" + sequenceLength;
+        
+
+        for (int i = 0; i < 24; i++)
+        {
+            colorStored = buttons[i % 6].GetComponent<MeshRenderer>().material.color;
+            buttons[i % 6].GetComponent<MeshRenderer>().material = lightTypes[1];
+            buttons[i % 6].GetComponent<MeshRenderer>().material.color = colorStored;
+            switch (i)
+            {
+                case 12:
+                    displays[0].text = "--";
+                    displays[1].text = "SI";
+                    break;
+                case 15:
+                    displays[0].text = "MO";
+                    displays[1].text = "N'";
+                    break;
+                case 18:
+                    displays[0].text = "SS";
+                    displays[1].text = "CY";
+                    break;
+                case 21:
+                    displays[0].text = "TA";
+                    displays[1].text = "LE";
+                    break;
+                default:
+                    break;
+            }
+            yield return new WaitUntil(() => elapsed >= (i + 1) * 20 / 140f);
+            for (int j = 0; j < 6; j++)
+            {
+                colorStored = buttonMaterials[j].GetComponent<MeshRenderer>().material.color;
+                buttonMaterials[j].material = lightTypes[0];
+                buttonMaterials[j].GetComponent<MeshRenderer>().material.color = colorStored;
+            }
+            //Debug.LogFormat("[Simon's Scytale #{0}] i={1}.", _moduleID, i);
+        }
+
+        // Solve the module
+        Module.HandlePass();
+        for (int j = 0; j < 6; j++)
+        {
+            colorStored = buttonMaterials[j].GetComponent<MeshRenderer>().material.color;
+            buttonMaterials[j].material = lightTypes[1];
+            buttonMaterials[j].GetComponent<MeshRenderer>().material.color = colorStored;
+        }
+
+        displays[0].text = "GG";
+        displays[1].text = "M8";
+
+        // Victory animation
+        yield return new WaitUntil(() => elapsed >= 26 * 20 / 140f);
+
+        string solvePositions = "123455432100"; // Infinitely loop through this string
+        int iteration = 0;
+
+        while (true)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                colorStored = buttonMaterials[j].GetComponent<MeshRenderer>().material.color;
+                buttonMaterials[j].material = lightTypes[0];
+                buttonMaterials[j].GetComponent<MeshRenderer>().material.color = colorStored;
+            }
+
+            int pos = _ALPHABET.IndexOf(solvePositions[iteration % 12]);
+            colorStored = buttonMaterials[pos].GetComponent<MeshRenderer>().material.color;
+            buttonMaterials[pos].material = lightTypes[1];
+            buttonMaterials[pos].GetComponent<MeshRenderer>().material.color = colorStored;
+
+            yield return new WaitUntil(() => elapsed >= (iteration + 27) * 20 / 140f);
+            iteration++;
+        }
     }
 
     // ==Internal Methods==
@@ -423,10 +649,75 @@ public class SimonsScytaleScript : MonoBehaviour {
         return scytale.Join("");
     }
 
+    // Keyboard support
+    private void EvaluateKeyPresses()
+    {
+        if (!moduleFocused || moduleSolved) return; // DO NOT evaluate key presses if solved or not focused.
+
+        for (int i = 0; i < TypableKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(TypableKeys[i]))
+            {
+                string color = colorNames[i]; // Get the name of the color
+                int index = Array.IndexOf(modColors, color);
+                buttons[index].OnInteract();
+                return;
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update () {
+
+        EvaluateKeyPresses();
+
+        // Rotate the scytale
         // Original: -180,0,0
         rotation = (rotation + (invertRotation ? -1f : 1f) * rotateSpeed);
         cylinder.transform.localEulerAngles = new Vector3(-180, 0, rotation);
+
+        if (moduleSolved)
+        {
+            rotateSpeed /= 1.002f;
+        }
 	}
+
+    // Twotch Plays
+
+    public IEnumerator ProcessTwitchCommand(string command)
+    {
+        yield return null;
+        command = command.ToUpperInvariant();
+
+        if (command == "COLORS")
+        {
+            yield return "sendtochat The colors are: " + modColors.Join(", ") + ".";
+        }
+        else
+        {
+            Match match = Regex.Match(command, "^[RGBCMY]{" + sequenceLength + "}$", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                twitchPlaysStruck = false;
+
+                foreach (char c in command)
+                {
+                    /*
+                    I need to take the color sequence, convert letters to strings, find their positions on the buttons, and then press accordingly.
+                    1. Take the color sequence and convert letters to strings.
+                    2. Find their positions on the buttons.
+                    3. Press accordingly.
+                     */
+                    string toPress = colorNames["RYGCBM".IndexOf(c)];
+                    KMSelectable button = buttons[Array.IndexOf(modColors, toPress)];
+                    button.OnInteract();
+                    yield return new WaitUntil(() => !isInteractable);
+                    yield return new WaitUntil(() => isInteractable);
+                    if (twitchPlaysStruck) yield break;
+                }
+                yield return "solve";
+            }
+            else yield return "sendtochaterror The given submission sequence is invalid!";
+        }
+    }
 }
